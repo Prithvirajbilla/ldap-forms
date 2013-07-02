ef2f93fabbe7e9a6d1decaaa22c648d1365af401
prithviraj billa <prithviraj.m.billa@gmail.com>
2013-07-02 22:12:36 +0530

    started app and added bootstrap

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Login Form</title>
+<link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css">
+<script type="text/javascript" href="/bootstrap/js/jquery.js"></script>
+<script type="text/javascript" href="/bootstrap/js/botstrap.min.js"></script>
+</head>
+<body>
+<?php echo "Hello world"; ?>
+
+</body>
+</html>
